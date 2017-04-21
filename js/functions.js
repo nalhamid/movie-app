a8880c1d48411d3ca9cf638304a0c5f315a880b2
nalhamid <nore.m.h@gmail.com>
2017-04-21 19:53:47 +0300

    link with son

--- js/functions.js
+++ js/functions.js
@@ -1,43 +1,39 @@
 $(document).ready(function() {
+
     var url = 'http://api.themoviedb.org/3/',
         mode = 'search/movie?query=',
         input,
         movieName,
-        topRated ='movie/top_rated',
-        popular = 'movie/popular'
-        key = '&api_key=bab007b9a6288af1455b8cee1f4f9d36';
+        topRated = 'movie/top_rated',
+        popular = 'movie/popular',
+        imagePath = 'https://image.tmdb.org/t/p/w342/'
+    key = '?api_key=bab007b9a6288af1455b8cee1f4f9d36';
 
-        $.getjson(url+popular+key+"?callback=?",function (json) {
-          if (json != "Nothing found."){
-            json.foreach(function(movie){
+    console.log("1");
 
-var movieBox = movieInfo.replace("%name%", movie.title).replace("%poster%",movie.poster_path);
+    $.ajax({
+        type: 'GET',
+        url: url + popular + key,
+        async: false,
+        jsonpCallback: 'testing',
+        contentType: 'application/json',
+        dataType: 'jsonp',
+        success: function(json) {
+            console.dir(json);
+            console.log("2");
+            console.log(json);
+            json.results.forEach(function(movie) {
+               
+                var movieBox = movieInfo.replace("%name%", movie.title).replace("%poster%", imagePath + movie.poster_path).replace("%rating%" , movie.vote_average);
 
-$("#popular-box:last").append(movieBox);
+                $("#popular-box:last").append(movieBox);
 
-            })
-          }
-        });
-
-    $('button').click(function() {
-        var input = $('#movie').val(),
-            movieName = encodeURI(input);
-        $.ajax({
-            type: 'GET',
-            url: url + mode + input + key,
-            async: false,
-            jsonpCallback: 'testing',
-            contentType: 'application/json',
-            dataType: 'jsonp',
-            success: function(json) {
-                console.dir(json);
-            },
-            error: function(e) {
-                console.log(e.message);
-            }
-        });
+            });
+        },
+        error: function(e) {
+            console.log(e.message);
+        }
     });
 
+});
 
-
-});​
--- js/helper.js
+++ js/helper.js
@@ -1,4 +1,6 @@
 
 
 
-var movieInfo = '<div class="movie"><div class="movie-image">  <a href="#"><span class="play"><span class="name">%name%</span></span><img src="%poster%" alt="movie" /></a></div><div class="rating">  <p>RATING</p> <div class="stars"> <div class="stars-in"> </div> </div> <span class="comments">12</span> </div> </div>';
+var movieInfo = '<div class="movie"><div class="movie-image">  <a href="#"><span class="play"><span class="name">%name%</span></span><img src="%poster%" alt="movie" /></a></div><div class="rating">  <p>RATING:  </p> <div> %rating% </div> </div> </div>';
+var trailers = ' <iframe width="560" height="315" src="https://www.youtube.com/embed/%key%" frameborder="0" allowfullscreen></iframe>';
+var reviews = '<div class="review"> <h3>%auther%</h3> <p>%content%</p> </div>'
